68ce52104850a30e7c8e12be691f510d322586b7
derrick <haylederrick14@gmail.com>
2025-04-09 03:26:31 +0300

    Modify db schema files

---
+++ db/migrate/20250408220345_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.text :bio
+
+      t.timestamps
+    end
+    add_index :users, :username
+    add_index :users, :email
+  end
+end
