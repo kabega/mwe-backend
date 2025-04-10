fa3500d942627060de8f6369e9526abe87c586ca
derrick <haylederrick14@gmail.com>
2025-04-10 17:12:00 +0300

    update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
       sessions: 'users/sessions',
       registrations: 'users/registrations'
     }
+    namespace :api do
+      namespace :v1 do
+        resources :job_listings, only: [:index, :create, :show, :update, :destroy]
+      end
+    end
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
