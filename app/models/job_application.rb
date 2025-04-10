74bd5835825ddbfd876768877828358e90dbe5be
derrick <haylederrick14@gmail.com>
2025-04-10 17:07:27 +0300

    Setup job listing model

---
+++ app/models/job_application.rb
@@ -0,0 +1,8 @@
+class JobApplication < ApplicationRecord
+  belongs_to :user
+  belongs_to :job_listing
+  enum status: {
+    applied: 'applied',
+    reviewed: 'reviewed',
+  }, default: 'applied'
+end
--- app/models/job_listing.rb
+++ app/models/job_listing.rb
@@ -1,3 +1,7 @@
 class JobListing < ApplicationRecord
   belongs_to :user
+  has_many :job_applications, dependent: :destroy
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
 end
