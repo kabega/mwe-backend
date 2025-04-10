c82c5aee8d5ffc58e91347749f1383716ccf5dd6
derrick <haylederrick14@gmail.com>
2025-04-10 13:43:18 +0300

    Modify schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_09_000509) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_10_103424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "job_listings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.string "status"
+    t.string "qualifications"
+    t.string "experience"
+    t.integer "salary"
+    t.string "salary_currency"
+    t.datetime "deadline"
+    t.bigint "user_id", null: false
+    t.string "job_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_job_listings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -33,4 +50,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_09_000509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username"
   end
+
+  add_foreign_key "job_listings", "users"
 end
