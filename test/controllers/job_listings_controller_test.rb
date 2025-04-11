8f3cdad7783d12d9032467378e3e62fbcd230573
derrick <haylederrick14@gmail.com>
2025-04-11 13:01:13 +0300

    Run tests on user model

--- test/controllers/job_listings_controller_test.rb
+++ test/controllers/job_listings_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class JobListingsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
--- test/models/job_listing_test.rb
+++ test/models/job_listing_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class JobListingTest < ActiveSupport::TestCase
   # test "the truth" do
