965c270b7335edf2344d8e8ea391fa8f0b046498
derrick <haylederrick14@gmail.com>
2025-04-11 16:53:54 +0300

    modify jobs data validations

--- app/models/job_listing.rb
+++ app/models/job_listing.rb
@@ -1,7 +1,17 @@
 class JobListing < ApplicationRecord
   belongs_to :user
   has_many :job_applications, dependent: :destroy
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :location, presence: true
+  validates :title, :description, :status, :location, :salary, :salary_currency, :deadline, :qualifications,
+            :experience, :job_type, presence: true
+  validate :deadline_must_be_in_future
+
+  private
+
+  def deadline_must_be_in_future
+    return if deadline.blank?
+
+    return unless deadline <= Date.current
+
+    errors.add(:deadline, 'must be in the future')
+  end
 end
