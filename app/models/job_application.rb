d8f9b795612d0d9f104e2a2aa8808f8a204b9838
derrick <haylederrick14@gmail.com>
2025-04-12 20:22:14 +0300

    Update job application model

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,8 +1,14 @@
 class JobApplication < ApplicationRecord
+  after_save :applicationcounter
   belongs_to :user
   belongs_to :job_listing
-  enum status: {
-    applied: 'applied',
-    reviewed: 'reviewed'
-  }, default: 'applied'
+  validates :resume, presence: true
+  validates :cover_letter, presence: true
+  validates :status, inclusion: {
+    in: %w[Submitted Under Review Accepted Rejected],
+      message: "%{value} is not a valid status. Use 'Submitted', 'Under Review', 'Accepted', or 'Rejected'"
+      }
+      def applicationcounter
+        job_listing.increment!(:application_counter)
+      end
 end
