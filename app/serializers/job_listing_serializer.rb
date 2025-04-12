c0c8eb92d0bcc4ae1f7e47bd0094c0d065b16ea5
derrick <haylederrick14@gmail.com>
2025-04-12 20:24:06 +0300

    Setup app serializer

--- app/serializers/job_listing_serializer.rb
+++ app/serializers/job_listing_serializer.rb
@@ -2,7 +2,7 @@ class JobListingSerializer
   include JSONAPI::Serializer
   attributes :id, :title, :description, :location, :status,
              :qualifications, :experience, :salary, :salary_currency,
-             :deadline, :job_type
+             :deadline, :job_type, :application_counter
 
   belongs_to :user
 end
