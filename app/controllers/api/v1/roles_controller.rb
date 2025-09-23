051705709c376e18a59b2f7845c31c09126c397b
derrick <haylederrick14@gmail.com>
2025-09-23 16:45:44 +0300

    Fix Render build script for API-only Rails app

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,2 +1,48 @@
-class RolesController < ApplicationController
+class Api::V1::RolesController < ApplicationController
+  before_action :set_role, only: %i[show update destroy]
+
+  # GET /roles
+  def index
+    @roles = Role.all
+    render json: @roles
+  end
+
+  # GET /roles/1
+  def show
+    render json: @role
+  end
+
+  # POST /roles
+  def create
+    @role = Role.new(role_params)
+    if @role.save
+      render json: @role, status: :created, location: api_v1_role_url(@role)
+    else
+      render json: @role.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /roles/1
+  def update
+    if @role.update(role_params)
+      render json: @role
+    else
+      render json: @role.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /roles/1
+  def destroy
+    @role.destroy
+  end
+
+  private
+
+  def set_role
+    @role = Role.find(params[:id])
+  end
+
+  def role_params
+    params.require(:role).permit(:name, :description)
+  end
 end
